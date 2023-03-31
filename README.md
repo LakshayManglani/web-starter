f4054dbcbb3d1809c9155472437bf5cb93863d7f
LakshayManglani <lakshaymanglani2212@gmail.com>
2023-03-31 19:42:15 +0530

    docs: Initial Commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Website Starter Pack for (HTML, CSS & JS)
+
+## Website ?
+1. What is you Website About?
+1. What does your Website Look Like?
---
+++ index.js
@@ -0,0 +1,14 @@
+let myLuckyNumber = 7;
+
+// todo :  Talk About the Green, Grey , Red Square Dots.
+
+// * Green
+myLuckyNumber;
+
+// * Grey
+function add(num1, num2) {
+    return num1 + num2;
+}
+
+// * Execution Error
+console.log(wow);
